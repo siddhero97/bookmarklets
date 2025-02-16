c30475b1be0fc6b47b144938f203edd61c536cce
give-me <48658661+give-me@users.noreply.github.com>
2025-02-16 20:10:18 +0300

    Optimize and comment the code

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,13 +2,11 @@
     let script = document.createElement('script');
     script.src = 'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.2/html2pdf.bundle.min.js';
     script.onload = function () {
-        let element = document.querySelector('div.flex-1.flex.flex-col.gap-3.px-4');
-        let timestamp = new Date().toISOString().replace(/[-:T.]/g, '').slice(0, 14);
-        html2pdf().set({
-            margin: 5,
-            filename: `${timestamp}.pdf`,
-            html2canvas: {scale: 2, logging: false}
-        }).from(element).save();
+        // Get a timestamp and an element with a dialog
+        let ts = new Date().toISOString().replace(/[-:T.]/g, '').slice(0, 14),
+            el = document.querySelector('div.flex.flex-1.flex-col.gap-3.px-4');
+        // Create a PDF from the element with a dialog
+        html2pdf().set({margin: 5, filename: `${ts}.pdf`, html2canvas: {scale: 2, logging: false}}).from(el).save();
     };
     document.body.appendChild(script);
 })();
