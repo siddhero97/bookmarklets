865cc5832517a26d602f408a0e025e9654d5778b
give-me <48658661+give-me@users.noreply.github.com>
2025-02-16 21:11:39 +0300

    Merge dialogs with artifacts if needed

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,9 +4,14 @@
     script.onload = function () {
         // Get a timestamp and an element with a dialog
         let ts = new Date().toISOString().replace(/[-:T.]/g, '').slice(0, 14),
-            el = document.querySelector('div.flex.flex-1.flex-col.gap-3.px-4');
+            e1 = document.querySelector('div.flex.flex-1.flex-col.gap-3.px-4'),
+            e2 = document.querySelector('div.flex.flex-1.overflow-x-auto.overflow-y-scroll');
         // Create a PDF from the element with a dialog
-        html2pdf().set({margin: 5, filename: `${ts}.pdf`, html2canvas: {scale: 2, logging: false}}).from(el).save();
+        let pdf = html2pdf().set({margin: 5, filename: `${ts}.pdf`, html2canvas: {scale: 2, logging: false}}).from(e1);
+        // Add the element with artifacts if open
+        e2 && (pdf = pdf.toPdf().get('pdf').then(pdfObj => pdfObj.addPage()).from(e2).toContainer().toCanvas().toPdf());
+        // Render the PDF
+        pdf.save();
     };
     document.body.appendChild(script);
 })();
